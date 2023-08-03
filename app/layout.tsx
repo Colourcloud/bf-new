047cb08c271b8cfb4930285a508e8c3fd86f877d
Jaromme <jarommelawn@gmail.com>
2023-08-04 04:10:59 +1200

    Remove boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Builtflat - Digital Agency, New Zealand',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="_next">{children}</body>
     </html>
   )
 }
