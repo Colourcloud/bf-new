8109314dfad465d72b2b59816d34e6d62a94a102
Jaromme <jarommelawn@gmail.com>
2023-08-04 04:15:21 +1200

    Yes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <div>Hello World</div>
+      <Link href='/dashboard'></Link>
     </>
   )
 }
