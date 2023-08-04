bf18cdc01c5e90c94e8f301ddf6bde002804b5ea
Jaromme <jarommelawn@gmail.com>
2023-08-04 16:45:23 +1200

    &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <div className="header-content flex flex-col spaced-m spaced-p w-full text-center items-center mx-auto lg:w-4/5">
             <div className="header-text flex flex-col">
               <h1 className="text-4xl purple-gradient leading-tight font-bold text-center md:text-7xl pb-4">Drive Business Growth with Cutting-Edge Digital Solutions</h1>
-              <h4 className="text-xl text-white text-center mt-6 lg:text-2xl">In a world driven by technology, it's important for your customers that your brand has a digital space on the web. Our team can make this happen.</h4>
+              <h4 className="text-xl text-white text-center mt-6 lg:text-2xl">In a world driven by technology, it&apos;s important for your customers that your brand has a digital space on the web. Our team can make this happen.</h4>
               </div>
               <div className="header-buttons flex flex-row gap-6 mt-8">
                 <Button backgroundColor="--primary-color" textColor="white" hoverColor="white" href="/application" >Free Consoltation</Button>
@@ -21,7 +21,7 @@ export default function Home() {
             </div>
         </div>
 
-        
+
 
       </header>
     </>
