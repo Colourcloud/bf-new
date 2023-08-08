e906816bce3052ee8069d5d690c6e6b9780ab2c6
Jaromme <jarommelawn@gmail.com>
2023-08-09 01:18:49 +1200

    commit

--- app/components/home/Collaboration.tsx
+++ app/components/home/Collaboration.tsx
@@ -10,12 +10,12 @@ const Collaboration = () => {
     return (
         <div className="bg-gradient-purple relative">
         <section className="builtflat-section spaced-p spaced-m">
-            <div className="site-wrapper flex flex-col gap-8">
-                <div className="section-text flex flex-col gap-8 w-3/5 mx-auto text-center items-center">
+            <div className="flex flex-col gap-8">
+                <div className="section-text flex flex-col gap-8 w-full md:w-3/5 md:mx-auto text-left md:text-center items-start md:items-center">
                     <span className='highlighted-purple-text rounded-full text-sm font-medium'>Collaboration</span>
-                    <h4 className='text-5xl font-bold'>A collaborative experience to speed up those busy workflows</h4>
+                    <h4 className='text-4xl md:text-5xl font-bold'>A collaborative experience to speed up those busy workflows</h4>
                     <p className='text-xl font-light'>We work closely with other design & development agencies, to help them meet their client deadlines. Leveraging workloads can greatly increase the productivity of a team, to deliver a more polished project.</p>
-                    <div className="user-icons flex flex-row">
+                    <div className="user-icons flex flex-row justify-start">
                         <Image src="/home/avatars/avatar-1.jpg" alt="face avatar" width="200" height="200" className='avatar-icon rounded-full mr-[-20px] relative z-10'></Image>
                         <Image src="/home/avatars/avatar-2.jpg" alt="face avatar" width="200" height="200" className='avatar-icon rounded-full mr-[-20px] relative z-20'></Image>
                         <Image src="/home/avatars/avatar-3.jpg" alt="face avatar" width="200" height="200" className='avatar-icon rounded-full mr-[-20px] relative z-30'></Image>
