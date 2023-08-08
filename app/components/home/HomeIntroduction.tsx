ec09d382dfcda3311c7bd7e23550ab01e5b04353
Jaromme <jarommelawn@gmail.com>
2023-08-09 03:39:03 +1200

    commit

--- app/components/home/HomeIntroduction.tsx
+++ app/components/home/HomeIntroduction.tsx
@@ -11,7 +11,7 @@ const HomeIntroduction = () => {
       <div className="builtflat-container-block">
       <div className="bg-gradient-purple">
       <div className="site-wrapper">
-        <section className="builtflat-section spaced-p spaced-m flex flex-col md:flex-row gap-9 items-center">
+        <section className="builtflat-section spaced-p spaced-m flex flex-col md:flex-row gap-9 items-center relative z-10">
               <div className="section-text flex flex-col gap-8">
                 <h2 className='text-4xl md:text-5xl font-bold'>We help businesses thrive through effective digital design</h2>
                 <p className='text-xl font-light'>Whether you want to grow brand awareness, showcase your services, increase sales, or increase user engagement, Builtflat can help. We understand it can be difficult to get your brand online, so let us do the hard work for you.</p>
