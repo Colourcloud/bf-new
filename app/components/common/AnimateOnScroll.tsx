5e1c4ae6323e807ca6edef3d4b7376065c93c346
Jaromme <jarommelawn@gmail.com>
2023-08-09 04:45:19 +1200

    com

--- app/components/common/AnimateOnScroll.tsx
+++ app/components/common/AnimateOnScroll.tsx
@@ -3,15 +3,18 @@ import { useInView } from 'react-intersection-observer';
 
 interface AnimateOnScrollProps {
   children: React.ReactNode;
+  disableRemoveClass?: boolean; // New prop to disable removing the "on-screen" class
 }
 
-const AnimateOnScroll: React.FC<AnimateOnScrollProps> = ({ children }) => {
+const AnimateOnScroll: React.FC<AnimateOnScrollProps> = ({ children, disableRemoveClass }) => {
   const [ref, inView] = useInView({
     threshold: 0.1,
   });
 
+  const shouldRemoveClass = !disableRemoveClass && !inView; // Check if the "on-screen" class should be removed
+
   return React.cloneElement(children as React.ReactElement, {
-    className: `${(children as React.ReactElement).props.className} animate ${inView ? 'on-screen' : ''}`,
+    className: `${(children as React.ReactElement).props.className} animate ${shouldRemoveClass ? '' : 'on-screen'}`,
     ref,
   });
 };
