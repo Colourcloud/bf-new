0f2cfed7a8681e1b1da097939ede19476ab66740
Jaromme <jarommelawn@gmail.com>
2023-08-09 03:19:37 +1200

    commit

--- app/components/home/Header.tsx
+++ app/components/home/Header.tsx
@@ -14,13 +14,13 @@ const Header = () => {
           <div className="site-wrapper">
             <div className="header-content flex flex-col spaced-m spaced-p w-full text-center items-center mx-auto lg:w-4/5">
               <div className="header-text flex flex-col">
-                <h1 className="text-white text-4xl leading-tight font-bold text-center md:text-7xl pb-4">Drive Business Growth with Cutting-Edge Digital Solutions</h1>
+                <h1 className="text-4xl purple-gradient leading-tight font-bold text-center md:text-7xl pb-4">Drive Business Growth with Cutting-Edge Digital Solutions</h1>
                 <h4 className="text-xl text-white text-center mt-6 lg:text-2xl">In a world driven by technology, it&apos;s important for your customers that your brand has a digital space on the web. Our team can make this happen.</h4>
                 </div>
-                {/* <div className="header-buttons flex flex-col sm:flex-row gap-6 mt-8">
+                <div className="header-buttons flex flex-col sm:flex-row gap-6 mt-8">
                   <Button backgroundColor="--primary-color" textColor="white" className="background-purple" href="/application" >Free Consoltation</Button>
                   <Button backgroundColor="--button-dark-bg" textColor="white"  className="background-dark" href="/">View Case Studies</Button>
-                </div> */}
+                </div>
               </div>
               <div className="header-images flex flex-row justify-center relative">
               <Image
