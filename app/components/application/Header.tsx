6efb345b21bf22abf9d8e7fdbad70754071df04c
Jaromme <jarommelawn@gmail.com>
2023-08-16 19:04:22 +1200

    fix spelling

--- app/components/application/Header.tsx
+++ app/components/application/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
                     <h1 className='text-4xl md:text-6xl font-extrabold'>Pushing your ideas into production ready solutions</h1>
                     <p className='text-xl font-light'>Whether you&apos;re building an e-commerce store, or a platform that uses a lot of data, we have you covered. Our experienced development teams can bring your ideas to life. Using the most robust and up to date technologies, we ensure your application is built with functionality and scalability in mind.</p>
                     <div className="header-buttons flex flex-col sm:flex-row gap-6 mt-8">
-                        <Button backgroundColor="--primary-color" textColor="white" className="background-purple" href="/application" >Free Consoltation</Button>
+                        <Button backgroundColor="--primary-color" textColor="white" className="background-purple" href="/application" >Free Consultation</Button>
                         <Button backgroundColor="--button-dark-bg" textColor="white"  className="background-dark" href="/">View Case Studies</Button>
                     </div>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Header from './components/home/Header'
 import Clients from './components/common/Clients'
 import Services from './components/home/Services'
