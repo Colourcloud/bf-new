4c666f9cc3b7cc94d0f011cd7be6bc151af800cb
Jaromme <jarommelawn@gmail.com>
2023-08-18 10:07:08 +1200

    c

--- app/components/home/HomeIntroduction.tsx
+++ app/components/home/HomeIntroduction.tsx
@@ -11,7 +11,7 @@ const HomeIntroduction = () => {
       <div className="bg-gradient-purple">
       <div className="site-wrapper">
         <section className="builtflat-section spaced-p spaced-m flex flex-col md:flex-row gap-9 items-center relative z-10">
-              <div className="section-text flex flex-col gap-8">
+              <div className="section-text flex flex-col gap-8 w-full lg:w-2/5">
                 <h2 className='text-4xl md:text-5xl font-bold'>We help businesses thrive through effective digital design</h2>
                 <p className='text-xl font-light'>Whether you want to grow brand awareness, showcase your services, increase sales, or increase user engagement, Builtflat can help. We understand it can be difficult to get your brand online, so let us do the hard work for you.</p>
                 <div className="section-buttons flex flex-row gap-4">
@@ -19,7 +19,7 @@ const HomeIntroduction = () => {
                 </div>
               </div>
               <div className="section-graphic">
-                <Image src="/home/test.jpg" alt="header-graphic" width="2700" height="500" className='rounded-lg'></Image>
+                <Image src="/home/test.jpg" alt="header-graphic" width="800" height="500" className='rounded-lg'></Image>
               </div>
         </section>
       </div>
