c0b35b5671838f3269f373c16b725c1815906f72
Jaromme <jarommelawn@gmail.com>
2023-10-26 01:26:38 +1300

    c

--- app/components/application/CmsSoftware.tsx
+++ app/components/application/CmsSoftware.tsx
@@ -5,7 +5,7 @@ const CmsSoftware = () => {
     return (
         <>
         <section className="builtflat-section">
-            <div className="container w-11/12 lg:w-2/3 mx-auto rounded-lg shadow-md bg-white border-gray-200 border p-8 md:p-10">
+            <div className="container w-11/12 xl:w-2/3 mx-auto rounded-lg shadow-md bg-white border-gray-200 border p-8 md:p-10">
                 <div className="section-text flex flex-col gap-4 w-full md:w-3/5">
                     <h4 className='text-3xl md:text-4xl font-bold'>Comfortable with an existing content management software?</h4>
                     <p className='font-light text-lg'>If you or your team already has experience with a popular CMS software, Builflat can develop on top of majority of the popular systems, allowing your team to continue using what they&apos;re comfortable with.</p>
