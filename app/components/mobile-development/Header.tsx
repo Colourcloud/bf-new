6079b4f47a86631155f1d0c1be2ff6d739a96f09
Jaromme <jarommelawn@gmail.com>
2024-03-18 21:45:29 +1300

    push

--- app/components/mobile-development/Header.tsx
+++ app/components/mobile-development/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
             <div className="header-content flex flex-col spaced-m spaced-p w-full text-center items-center mx-auto lg:w-4/5">
               <div className="header-text flex flex-col">
                 <h1 className="text-4xl purple-gradient leading-tight font-semibold text-center md:text-7xl pb-4">Connect with your users on any device with your own mobile application</h1>
-                <h4 className="text-xl text-white text-center mt-6 lg:text-2xl">Empower your brand to thrive in the palm of your users' hands. Building custom mobile apps that deliver value and drive interaction, ensuring a seamless connection on every device.</h4>
+                <h4 className="text-xl text-white text-center mt-6 lg:text-2xl">Empower your brand to thrive in the palm of your users hands. Building custom mobile apps that deliver value and drive interaction, ensuring a seamless connection on every device.</h4>
                 </div>
                 <div className="header-buttons flex flex-col sm:flex-row gap-6 mt-8">
                   <Button backgroundColor="--primary-color" textColor="white" className="background-purple" href="/contact" >Free Consultation</Button>
