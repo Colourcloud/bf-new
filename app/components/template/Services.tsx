9ed545c1c6c78fec7f431202ac450e6069ca16e0
Colourcloud <jarommelawn@gmail.com>
2024-06-19 15:31:32 +1200

    fix load image

--- app/components/template/Services.tsx
+++ app/components/template/Services.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import Button from '../common/Button';
 import AnimateOnScroll from '../common/AnimateOnScroll';
 import AnimatedText from '../common/AnimateText';
-
 import { useEffect, useState } from 'react';
 
 const images = [
@@ -13,11 +12,6 @@ const images = [
     '/template-design/image2.jpg',
     '/template-design/image3.jpg',
 ];
-
-// Define a type for the image map
-type ImageMap = {
-    [key: string]: string;
-};
 
 const Services = () => {
     const [currentImage, setCurrentImage] = useState('/template-design/image1.jpg');
@@ -65,6 +59,20 @@ const Services = () => {
     return (
         <div className='bg-[--dark-background-color]'>
             <div className="site-wrapper">
+                {/* Hidden images to preload */}
+                <div className="hidden">
+                    {images.map((src, index) => (
+                        <Image
+                            key={index}
+                            src={src}
+                            alt={`Preload ${index}`}
+                            priority={true}
+                            width={1}
+                            height={1}
+                        />
+                    ))}
+                </div>
+
                 <div className="content-container hidden md:flex invisible md:visible flex-row h-[2800px] gap-10 w-full" id="content-container">
                     <div className="content-text w-2/5 h-full flex flex-col gap-80">  {/* Text down the page */}
                         <div className="text-block flex flex-col gap-6 min-h-[600px] justify-center">
@@ -78,47 +86,55 @@ const Services = () => {
                             <span id="block-2"></span>
                         </div>
                         <div className="text-block flex flex-col gap-6 min-h-[700px] justify-center">
-                        <span id="block-3"></span>
+                            <span id="block-3"></span>
                             <AnimatedText><h4 className='text-white text-5xl font-bold'>Responsive in mind. Designed to work on any screen size</h4></AnimatedText>
                             <p className='text-white text-lg'>We&apos;re constantly improving and upgrading our templates and components to make sure they stay up to date with the ever-evolving digital world.</p>
                         </div>
                     </div>
                     <div className="content-graphic-container w-2/3  h-full p-8"> {/* Image that scrolls down the page */}
-                    <div className='sticky top-8 flex justify-center items-center'>
-                    <span id="trigger" className='w-2 h-2'></span>
-                    <AnimateOnScroll><Image src={currentImage} alt="yes" className={`w-full rounded-lg template-service-image scale ${flicker ? 'flicker' : ''}`} width="1200" height="1200" priority={true}></Image></AnimateOnScroll>
-                    </div>
+                        <div className='sticky top-8 flex justify-center items-center'>
+                            <span id="trigger" className='w-2 h-2'></span>
+                            <AnimateOnScroll>
+                                <Image src={currentImage} alt="yes" className={`w-full rounded-lg template-service-image scale ${flicker ? 'flicker' : ''}`} width="1200" height="1200" priority={true}></Image>
+                            </AnimateOnScroll>
+                        </div>
                     </div>
                 </div>
 
-                {/* MOBILE VIEW*/}
+                {/* MOBILE VIEW */}
 
                 <section className='mobile-services-layout pb-40 visible md:invisible flex gap-16 flex-col md:hidden'>
                     <div className="content-block flex flex-col gap-8">
-                        <AnimateOnScroll><Image src={images[0]} alt="yes" className='w-full rounded-lg template-service-image' width="1200" height="1200"></Image></AnimateOnScroll>
+                        <AnimateOnScroll>
+                            <Image src={images[0]} alt="yes" className='w-full rounded-lg template-service-image' width="1200" height="1200"></Image>
+                        </AnimateOnScroll>
                         <div className="content-text flex flex-col gap-6">
                             <AnimatedText><h4 className='text-white text-3xl font-bold'>Build with designed & coded templates. Ready for production</h4></AnimatedText>
                             <p className='text-white text-lg'>Just because they&apos;re pre-built does not mean you lose the freedom to make your website your own. Our template websites are still highly customisable to suit your brand and capture your uniqueness.</p>
                         </div>
                     </div>
                     <div className="content-block flex flex-col gap-8">
-                        <AnimateOnScroll><Image src={images[1]} alt="yes" className='w-full rounded-lg template-service-image' width="1200" height="1200"></Image></AnimateOnScroll>
+                        <AnimateOnScroll>
+                            <Image src={images[1]} alt="yes" className='w-full rounded-lg template-service-image' width="1200" height="1200"></Image>
+                        </AnimateOnScroll>
                         <div className="content-text flex flex-col gap-6">
                             <AnimatedText><h4 className='text-white text-3xl font-bold'>Tailor made design components for any project</h4></AnimatedText>
                             <p className='text-white text-lg'>We have a large collection of pre-designed elements & templates that have already been tried & tested for a large range of different industries. This allows our clients to save time & cost for their projects</p>
                         </div>
                     </div>
                     <div className="content-block flex flex-col gap-8">
-                        <AnimateOnScroll><Image src={images[2]} alt="yes" className='w-full rounded-lg template-service-image' width="1200" height="1200"></Image></AnimateOnScroll>
+                        <AnimateOnScroll>
+                            <Image src={images[2]} alt="yes" className='w-full rounded-lg template-service-image' width="1200" height="1200"></Image>
+                        </AnimateOnScroll>
                         <div className="content-text flex flex-col gap-6">
                             <AnimatedText><h4 className='text-white text-3xl font-bold'>Responsive in mind. Designed to work on any screen size</h4></AnimatedText>
                             <p className='text-white text-lg'>We&apos;re constantly improving and upgrading our templates and components to make sure they stay up to date with the ever-evolving digital world.</p>
                         </div>
                     </div>
                 </section>
-
             </div>
-            </div>
+        </div>
     );
 };
+
 export default Services;
