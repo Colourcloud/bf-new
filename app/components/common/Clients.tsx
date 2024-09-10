86dde7d7ccd10a165a4a4d065563fd9c391f2c11
Colourcloud <jarommelawn@gmail.com>
2024-09-11 01:09:11 +1200

    new client logos

--- app/components/common/Clients.tsx
+++ app/components/common/Clients.tsx
@@ -5,14 +5,14 @@ const Clients = () => {
     return (
         <>
         <div className="site-wrapper spaced-p spaced-m">
-            <div className="client-list w-full md:w-3/5 mx-auto text-center">
+            <div className="client-list w-full lg:w-4/5 mx-auto text-center">
                 <h4 className='text-2xl md:text-3xl font-medium'>Some of the awesome clients we&apos;ve worked with</h4>
-                <div className="brand-container spaced-p flex flex-wrap justify-center items-center gap-6 lg:gap-0 md:justify-between ">
-                   <Image src="/clients/kpmg.svg" alt="kpmg logo" width='100' height='44'></Image>
-                   <Image src="/clients/mercedes.svg" alt="kpmg logo" width='100' height='44'></Image>
-                   <Image src="/clients/pg.svg" alt="kpmg logo" width='100' height='44'></Image>
-                   <Image src="/clients/pinterest.svg" alt="kpmg logo" width='100' height='44'></Image>
-                   <Image src="/clients/stripe.svg" alt="kpmg logo" width='100' height='44'></Image>
+                <div className="brand-container spaced-p flex flex-wrap justify-center items-center gap-6 lg:gap-2 md:justify-between ">
+                   <Image src="/clients/johnstons.svg" alt="johnstons logo" width='100' height='44'></Image>
+                   <Image src="/clients/cvcompton.svg" alt="cvcompton logo" width='100' height='44'></Image>
+                   <Image src="/clients/wrks.svg" alt="wrks logo" width='100' height='44'></Image>
+                   <Image src="/clients/blankcanvas.png" alt="blankcanvas logo" width='100' height='44'></Image>
+                   <Image src="/clients/chasnz.svg" alt="chasnz logo" width='80' height='44'></Image>
                    <Image src="/clients/doofy.svg" alt="kpmg logo" width='100' height='44'></Image>
                 </div>
             </div>
