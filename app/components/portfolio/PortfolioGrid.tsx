98682da01f9a771a37194f08d7731c82e5035a9c
Colourcloud <jarommelawn@gmail.com>
2024-09-10 21:26:43 +1200

    fix padding on portfolio

--- app/components/portfolio/PortfolioGrid.tsx
+++ app/components/portfolio/PortfolioGrid.tsx
@@ -4,7 +4,7 @@ import portfolioData from '../../data/portfolioData.json';
 const PotrfolioGrid: React.FC = () => {
     return (
         <section className='portfolio-page bg-[--dark-background-color] spaced-p'>
-            <div className="max-w-[1600px] m-auto">
+            <div className="max-w-[1600px] m-auto px-4">
                 <div className="portfolio-grid grid grid-cols-1 md:grid-cols-2 gap-8">
                 {portfolioData.map((item) => (
                      <PortfolioCard key={item.id} id={item.id} title={item.title} image={item.image} slug={item.slug} />
