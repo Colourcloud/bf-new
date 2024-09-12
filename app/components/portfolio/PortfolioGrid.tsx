a868b02fb7f306882811284abf0125fc2555c754
Colourcloud <jarommelawn@gmail.com>
2024-09-12 19:39:33 +1200

    reverse the order for portfolio to show newest items first

--- app/components/portfolio/PortfolioGrid.tsx
+++ app/components/portfolio/PortfolioGrid.tsx
@@ -6,8 +6,8 @@ const PotrfolioGrid: React.FC = () => {
         <section className='portfolio-page bg-[--dark-background-color] spaced-p'>
             <div className="max-w-[1600px] m-auto px-4">
                 <div className="portfolio-grid grid grid-cols-1 md:grid-cols-2 gap-8">
-                {portfolioData.map((item) => (
-                     <PortfolioCard key={item.id} id={item.id} title={item.title} image={item.image} slug={item.slug} />
+                {portfolioData.slice().reverse().map((item) => (
+                        <PortfolioCard key={item.id} id={item.id} title={item.title} image={item.image} slug={item.slug} />
                 ))}
                 </div>
             </div>
