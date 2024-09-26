bc6e2545af9be325038194b26864f0a8526846e2
Colourcloud <jarommelawn@gmail.com>
2024-09-26 21:13:02 +1200

    partners

--- app/components/partnership/Partners.tsx
+++ app/components/partnership/Partners.tsx
@@ -12,13 +12,12 @@ const Partners = () => {
         <div className="site-wrapper flex flex-col gap-6">
             <h4 className='text-[--text-on-dark] font-semibold text-large text-center'>Our Design Partners</h4>
             <div className="partner-logos flex flex-row flex-wrap gap-14 justify-center">
-                <Image src="/partnership/brands/amazon.svg" alt="face avatar" width="100" height="125" className=''></Image>
-                <Image src="/partnership/brands/shopify.svg" alt="face avatar" width="100" height="125" className=''></Image>
-                <Image src="/partnership/brands/facebook.svg" alt="face avatar" width="100" height="125" className=''></Image>
-                <Image src="/partnership/brands/adobe.svg" alt="face avatar" width="100" height="125" className=''></Image>
-                <Image src="/partnership/brands/microsoft.svg" alt="face avatar" width="100" height="125" className=''></Image>
-                <Image src="/partnership/brands/disney.svg" alt="face avatar" width="75" height="125" className=''></Image>
-                <Image src="/partnership/brands/lyft.svg" alt="face avatar" width="40" height="125" className=''></Image>
+                <Image src="/partnership/brands/amazon.svg" alt="amazon logo" width="100" height="125" className=''></Image>
+                <Image src="/partnership/brands/shopify.svg" alt="shopify logo" width="100" height="125" className=''></Image>
+                <Image src="/partnership/brands/strapi.svg" alt="strapi logo" width="100" height="125" className=''></Image>
+                <Image src="/partnership/brands/adobe.svg" alt="adobe logo" width="100" height="125" className=''></Image>
+                <Image src="/partnership/brands/payload.svg" alt="payload logo" width="120" height="125" className=''></Image>
+                <Image src="/partnership/brands/doofy.svg" alt="doofy logo" width="100" height="125" className=''></Image>
             </div>
         </div>
        </section>
