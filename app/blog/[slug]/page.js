63afbbe891c1be59dca02aa8458eed33966a6250
Colourcloud <jarommelawn@gmail.com>
2024-10-24 15:22:43 +1300

    author on blogs

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -50,6 +50,9 @@ export default async function BlogPost({ params }) {
     year: 'numeric',
   });
 
+  // Get author information from _embedded data
+  const author = posts._embedded?.['author']?.[0];
+
   return (
     <>
       <div className='bg-[--dark-background-color]'>
@@ -61,7 +64,25 @@ export default async function BlogPost({ params }) {
               <div className='blog-intro flex flex-col gap-6 w-full lg:w-[80%] m-auto'>
                 <h1 className='text-white text-5xl font-bold'>{posts.title.rendered}</h1>
                 <span className='text-[--text-on-dark] text-xl' dangerouslySetInnerHTML={{ __html: posts.excerpt.rendered }}></span>
-                <span className='text-[--text-on-dark] italic'>Post created on: {readableDate}</span>
+                
+                {/* Author information */}
+                {author && (
+                  <div className="flex items-center gap-4 border-y border-[--border-colour-dark] py-4">
+                    <div className="relative h-12 w-12 overflow-hidden rounded-full">
+                      <Image 
+                        src={author.avatar_urls?.['96'] || '/default-avatar.png'}
+                        alt={`${author.name}'s profile picture`}
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-white font-medium">Written by {author.name}</span>
+                      <span className='text-[--text-on-dark] italic text-sm'>Posted on {readableDate}</span>
+                    </div>
+                  </div>
+                )}
+
                 <div className=''>
                   <Image src={posts._embedded?.['wp:featuredmedia']?.[0]?.source_url} alt="" width='1000' height='400' className='border border-[--border-colour-dark]'></Image>
                 </div>
@@ -76,7 +97,7 @@ export default async function BlogPost({ params }) {
                         <h2 className='text-4xl md:text-3xl font-bold'>Frequently Asked Questions</h2>
                         <p className='text-base font-light'>Below are some frequently asked questions relating to this blog post.</p>
                       </div>
-                      <Accordion type="single" collapsible>
+                      <Accordion type="single" collapsible className='mt-12'>
                         {posts.acf.faq
                           .filter(faqItem => faqItem.question.trim() !== '' || faqItem.answer.trim() !== '')
                           .map((faqItem, index) => (
@@ -92,7 +113,6 @@ export default async function BlogPost({ params }) {
                     </>
                   )}
                 </div>
-
               </div>
             </div>
           </div>
