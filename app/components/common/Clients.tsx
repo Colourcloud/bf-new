504c68d33682977fff8a3550e3d1ad5a614c2b3a
Colourcloud <jarommelawn@gmail.com>
2024-11-19 20:52:03 +1300

    KWT logo

--- app/components/common/Clients.tsx
+++ app/components/common/Clients.tsx
@@ -8,7 +8,7 @@ const Clients = () => {
             <div className="client-list w-full lg:w-4/5 mx-auto text-center">
                 <h4 className='text-2xl md:text-3xl font-medium'>Some of the awesome clients we&apos;ve worked with</h4>
                 <div className="brand-container spaced-p flex flex-wrap justify-center items-center gap-6 lg:gap-2 md:justify-between ">
-                   <Image src="/clients/johnstons.svg" alt="johnstons logo" width='125' height='44'></Image>
+                   <Image src="/clients/kwt.svg" alt="Kiwi wonder tours logo" width='100' height='44'></Image>
                    <Image src="/clients/cvcompton.svg" alt="cvcompton logo" width='100' height='44'></Image>
                    <Image src="/clients/wrks.svg" alt="wrks logo" width='100' height='44'></Image>
                    <Image src="/clients/blankcanvas.png" alt="blankcanvas logo" width='100' height='44'></Image>
