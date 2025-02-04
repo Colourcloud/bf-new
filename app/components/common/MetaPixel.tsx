7788d07a49136ee1bac75f7bb3b03f309a1e625c
Colourcloud <jarommelawn@gmail.com>
2025-02-04 13:52:16 +1300

    push

--- app/components/common/MetaPixel.tsx
+++ app/components/common/MetaPixel.tsx
@@ -1,24 +1,40 @@
 'use client';
 
 import Script from 'next/script';
+import { useEffect } from 'react';
 
 export default function MetaPixel() {
+  useEffect(() => {
+    // Check if fbq is initialized
+    if ((window as any).fbq) {
+      (window as any).fbq('track', 'PageView');
+    }
+  }, []);
+
   return (
     <>
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1807734113341234');
-          fbq('track', 'PageView');
-        `}
-      </Script>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1807734113341234');
+            fbq('track', 'PageView');
+            console.log('Meta Pixel initialized');
+          `,
+        }}
+        onLoad={() => {
+          console.log('Meta Pixel script loaded');
+        }}
+      />
       <noscript>
         <img 
           height="1" 
